504c0cf7a056abc0018ffabc23cacb528bc9d456
Yevhen Malyi <yevhen.malyi@indeema.com>
2023-07-28 15:48:25 +0300

    add basic structure

---
+++ src/core/router.tsx
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+} from 'react-router-dom';
+
+import { FavoritesPage, SearchPage } from '../pages';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: SearchPage,
+  },
+  {
+    path: '/search',
+    Component: FavoritesPage,  
+  }
+]);
