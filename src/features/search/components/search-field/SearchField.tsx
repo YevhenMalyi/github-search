80cf397b74ba470347a777e567f66ce8ba49017f
Yevhen Malyi <yevhen.malyi@indeema.com>
2023-07-29 14:35:41 +0300

    connected tailwind

--- src/features/search/components/search-field/SearchField.tsx
+++ src/features/search/components/search-field/SearchField.tsx
@@ -2,6 +2,6 @@ import { TextField } from '@mui/material';
 
 export const SearchField = () => {
   return (
-    <TextField id="outlined-search" label="Search field" type="search" />
+    <TextField label="Search field" type="search" />
   );
 };
